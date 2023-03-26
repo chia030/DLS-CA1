8bfb4ff271c691581e2f1dc545618783922d796b
chia <chiaravisca@outlook.com>
2023-03-26 21:50:01 +0200

    load balancer setup

---
+++ LoadBalancer/LoadBalancer/ILoadBalancerStrategy.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace LoadBalancer.LoadBalancer
+{
+    public interface ILoadBalancerStrategy
+    {
+        public string NextService(List<string> services);
+    }
+}
